0c3ee6244b1731dcf795857b6a6627c325aa706f
huangjinrrrrr <1413127940@qq.com>
2023-07-01 21:50:43 +0800

    3-6 增加ebook表的列表查询和通用的返回来CommonResp

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,29 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.pojo.Ebook;
+import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Autowired
+    private EbookService ebookService;
+
+
+    @GetMapping("/list")
+    public CommonResp EbookList(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
+    }
+
+}
---
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.mapper.EbookMapper;
+import com.jiawa.wiki.pojo.Ebook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Autowired
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
