5a787f2c38a0d0dde12bb2777aa860e85a8d4385
huangjinrrrrr <1413127940@qq.com>
2023-07-05 00:11:32 +0800

    6-12 增加按名字查询电子书功能

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -3,28 +3,23 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-<!--      <p>-->
-<!--        <a-form layout="inline" :model="param">-->
-<!--          <a-form-item>-->
-<!--            <a-input v-model:value="param.name" placeholder="名称">-->
-<!--            </a-input>-->
-<!--          </a-form-item>-->
-<!--          <a-form-item>-->
-<!--            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">-->
-<!--              查询-->
-<!--            </a-button>-->
-<!--          </a-form-item>-->
-<!--          <a-form-item>-->
-<!--            <a-button type="primary" @click="add()">-->
-<!--              新增-->
-<!--            </a-button>-->
-<!--          </a-form-item>-->
-<!--        </a-form>-->
-<!--      </p>-->
       <p>
-        <a-button type="primary" @click="add" size="large">
-          新增
-        </a-button>
+        <a-form layout="inline" :model="param">
+          <a-form-item>
+            <a-input v-model:value="param.name" placeholder="名称">
+            </a-input>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="handleQuery({page: 1, size: pagination.pageSize})">
+              查询
+            </a-button>
+          </a-form-item>
+          <a-form-item>
+            <a-button type="primary" @click="add">
+              新增
+            </a-button>
+          </a-form-item>
+        </a-form>
       </p>
       <a-table
           :columns="columns"
@@ -172,7 +167,8 @@ export default defineComponent({
       axios.get("/ebook/list",{
         params: {
           page: params.page,
-          size: params.size
+          size: params.size,
+          name: param.value.name
         }
       }).then((response) => {
         loading.value = false;
@@ -256,11 +252,13 @@ export default defineComponent({
     });
 
     return {
+      param,
       ebooks,
       pagination,
       columns,
       loading,
       handleTableChange,
+      handleQuery,
 
       edit,
       add,
