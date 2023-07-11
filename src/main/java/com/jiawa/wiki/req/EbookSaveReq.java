f296936c54cfc0c260419792a84493a218583f8d
huangjinrrrrr <1413127940@qq.com>
2023-07-11 18:00:37 +0800

    修复bug 将@NotNull改为@NotEmpty，表示不能为null和空串

--- src/main/java/com/jiawa/wiki/req/EbookSaveReq.java
+++ src/main/java/com/jiawa/wiki/req/EbookSaveReq.java
@@ -1,13 +1,13 @@
 package com.jiawa.wiki.req;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public class EbookSaveReq {
     private Long id;
 
-    @NotNull(message = "[名称]不能为空")
-    @NotBlank(message = "[名称]不能为空串")
+    @NotEmpty(message = "[名称]不能为空")
     private String name;
 
     private Long category1Id;
--- src/main/java/com/jiawa/wiki/req/UserRestPasswordReq.java
+++ src/main/java/com/jiawa/wiki/req/UserRestPasswordReq.java
@@ -1,11 +1,12 @@
 package com.jiawa.wiki.req;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 public class UserRestPasswordReq {
     private Long id;
 
-    @NotNull(message = "[密码]不能为空")
+    @NotEmpty(message = "[密码]不能为空")
 //    @Length(min = 6,max = 20,message = "[密码]必须是6~20位")
 //    @Pattern(regexp = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$",message = "[密码]至少包含 数字和英文，长度6~32")
     private String password;
