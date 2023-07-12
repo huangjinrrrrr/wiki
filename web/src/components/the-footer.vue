f0ae45cc6c334b79f0988a2f10778b33702be217
huangjinrrrrr <1413127940@qq.com>
2023-07-12 22:57:41 +0800

    9-9 使用vuex保存用户信息，方便其他组件获取登录信息，遗留问题：刷新页面会清除信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,13 +1,23 @@
 <template>
   <a-layout-footer style="text-align: center">
-    甲蛙电子书 ©2023
+    甲蛙电子书 ©2023 {{user.name}}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
   name: 'the-footer',
+  setup() {
+    const user = computed(() =>
+      store.state.user
+    );
+    return {
+      user
+    }
+
+  }
 });
 </script>
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -52,6 +52,7 @@
 import { defineComponent,ref } from 'vue';
 import axios from "axios";
 import { message } from "ant-design-vue";
+import store from "@/store";
 
 declare let hexMd5: any;
 declare let KEY: any;
@@ -88,6 +89,8 @@ export default defineComponent({
           message.success("登录成功");
 
           user.value = data.content;
+
+          store.commit("setUser",user.value)
         } else {
           message.error(data.message);
         }
