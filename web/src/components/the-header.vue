3beb70e39961af9423f9d8dfa06497b2db224b4e
huangjinrrrrr <1413127940@qq.com>
2023-07-12 22:33:19 +0800

    9-9 前端显示登录昵称，遗留问题：刷新页面会清除信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,8 +1,11 @@
 <template>
   <a-layout-header class="header">
     <div class="logo" />
-    <a class="login-menu"  @click="showLoginModal">
+    <a class="login-menu" v-show="!user.id" @click="showLoginModal">
       <span>登录</span>
+    </a>
+    <a class="login-menu" v-show="user.id">
+      <span>您好：{{user.name}}</span>
     </a>
     <a-menu
         theme="dark"
@@ -56,9 +59,14 @@ declare let KEY: any;
 export default defineComponent({
   name: 'the-header',
   setup() {
-    const loginUser =ref({
+    //登录后保存
+    const user = ref();
+    user.value = {}
+
+    //用来登录
+    const loginUser = ref({
       loginName: "test",
-      password: "test"
+      password: "test123"
     });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
@@ -78,6 +86,8 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登录成功");
+
+          user.value = data.content;
         } else {
           message.error(data.message);
         }
@@ -89,7 +99,8 @@ export default defineComponent({
       loginModalLoading,
       showLoginModal,
       login,
-      loginUser
+      loginUser,
+      user
     }
   }
 });
