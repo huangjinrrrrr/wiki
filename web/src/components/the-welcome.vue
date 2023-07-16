dd7cf5e41bae3f9421dfaf0b858a4312d6edafcf
huangjinrrrrr <1413127940@qq.com>
2023-07-16 18:43:41 +0800

    11-6 将欢迎文字提取成组件

---
+++ web/src/components/the-welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>欢迎使用wiki知识库1</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-welcome',
+  setup() {
+
+    return {
+
+    }
+  }
+});
+</script>
