3f067ca10884ce368bf88bbcbec809d78e4c3559
huangjinrrrrr <1413127940@qq.com>
2023-07-16 18:37:09 +0800

    11-6 后端增加获取统计数值接口

--- src/main/java/com/jiawa/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/jiawa/wiki/service/EbookSnapshotService.java
@@ -1,8 +1,11 @@
 package com.jiawa.wiki.service;
 
 import com.jiawa.wiki.mapper.EbookSnapshotMapperCust;
+import com.jiawa.wiki.resp.StatisticResp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -12,5 +15,9 @@ public class EbookSnapshotService {
 
     public void genSnapshot() {
         ebookSnapshotMapperCust.genSnapshot();
+    }
+
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
     }
 }
