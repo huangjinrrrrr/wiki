56071c3584a2afeec9c4bdc796f93f62eb801ead
huangjinrrrrr <1413127940@qq.com>
2023-07-16 23:19:57 +0800

    修改关于我们

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,30 @@
 <template>
+  <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+
   <div class="about">
-    <h1>about 关于我们</h1>
+    <h1>关于我们</h1>
+    <div>
+      <b>项目：
+        <a href="" target="_blank">
+          SpringBoot+Vue3 WIKI知识库系统
+        </a>
+      </b>
+    </div>
   </div>
+  </a-layout-content>
 </template>
+<style>
+.about {
+  line-height: 30px;
+  padding: 40px;
+  background-color: #f5f5f5;
+  font-size: 16px;
+}
+.about h2 {
+  margin-bottom: 30px;
+  font-weight: 700;
+}
+.about img {
+  margin-top: 10px;
+}
+</style>
